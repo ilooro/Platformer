503622a1655a0fda2cd9af4511f42ef5a072aa2b
Ilya <sdserver4@outlook.com>
2024-01-03 01:31:21 +0700

    Add project files.

---
+++ Platformer/Classes/Hero.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+using System.Windows;
+using System.Numerics;
+using System.Windows.Controls;
+
+namespace Platformer.Classes
+{
+    internal class Hero
+    {
+        private int _speed;
+        private int _jumpSpeed;
+        private int _jumpForce;
+
+        public int CurrentHorOffset { get; private set; } = 0;
+        public int CurrentVerOffset { get; private set; } = 0;
+
+        private int _currentJumpForce = 0;
+        private bool _isJumping = false;
+
+        private int _inputHorOffset = 0;
+        private Rect _prevHeroBox = new Rect(0, 0, 0, 0);
+
+        public Hero(int speed, int jumpSpeed, int jumpForce) 
+        {
+            _speed = speed;
+            _jumpSpeed = jumpSpeed;
+            _jumpForce = jumpForce;
+        }
+
+        public void Update(ref Rect heroBox, List<Rect> boxes)
+        {
+            if (_isJumping && _currentJumpForce <= 0)
+                _isJumping = false;
+            if (_isJumping)
+            {
+                CurrentVerOffset = -_jumpSpeed;
+                _currentJumpForce--;
+            }
+            else
+            {
+                CurrentVerOffset = _jumpSpeed;
+            }
+
+            CurrentHorOffset = _inputHorOffset;
+            foreach (Rect box in boxes)
+            {
+                if (heroBox.IntersectsWith(box))
+                {
+                    if (_prevHeroBox.Bottom < box.Top && heroBox.Bottom > box.Top)
+                    {
+                        CurrentVerOffset = (int)(box.Top - heroBox.Bottom);
+                    }
+                    if (!_isJumping && heroBox.Bottom == box.Top)
+                    {
+                        CurrentVerOffset = 0;
+                    }
+
+                    if (_prevHeroBox.Right <= box.Left)
+                    {
+                        if (heroBox.Right > box.Left)
+                        {
+                            CurrentHorOffset = (int)(box.Left - heroBox.Right);
+                        }
+                        else if (heroBox.Right == box.Left && CurrentHorOffset > 0)
+                        {
+                            CurrentHorOffset = 0;
+                        }
+                    }
+                    else if (_prevHeroBox.Left >= box.Right)
+                    {
+                        if (heroBox.Left < box.Right)
+                        {
+                            CurrentHorOffset = (int)(box.Right - heroBox.Left);
+                        }
+                        else if (heroBox.Left == box.Right && CurrentHorOffset < 0)
+                        {
+                            CurrentHorOffset = 0;
+                        }
+                    }
+                }
+            }
+            _prevHeroBox = heroBox;
+        }
+
+        public void MoveLeft()
+        {
+            _inputHorOffset = -_speed;
+        }
+
+        public void MoveRight()
+        {
+            _inputHorOffset = _speed;
+            //_inputHorOffset = _rightWall ? 0 : _speed;
+        }
+
+        public void StopMoving()
+        {
+            _inputHorOffset = 0;
+        }
+
+        public void Jump()
+        {
+            if (!_isJumping)
+            {
+                _isJumping = true;
+                _currentJumpForce = _jumpForce;
+            }
+        }
+    }
+}
---
+++ Platformer/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Platformer.Classes;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Platformer
+{
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer Timer = new DispatcherTimer();
+
+        Hero GameHero;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Initialization
+
+            Canvas.Focus();
+            Timer.Tick += GameEngine;
+            Timer.Interval = TimeSpan.FromMilliseconds(10);
+
+            StartGame();
+        }
+
+        private void GameEngine(object? sender, EventArgs e)
+        {
+            Rect playerHitBox = new Rect(Canvas.GetLeft(player), Canvas.GetTop(player), player.Width, player.Height);
+            var boxes = new List<Rect>();
+            IEnumerable<Rectangle> rectangles = Canvas.Children.OfType<Rectangle>();
+            foreach (var rect in rectangles)
+                if (rect != player)
+                    boxes.Add(new Rect(Canvas.GetLeft(rect), Canvas.GetTop(rect), rect.Width, rect.Height));
+
+            GameHero.Update(ref playerHitBox, boxes);
+
+            Canvas.SetTop(player, Canvas.GetTop(player) + GameHero.CurrentVerOffset);
+            Canvas.SetLeft(player, Canvas.GetLeft(player) + GameHero.CurrentHorOffset);
+        }
+
+        private void StartGame()
+        {
+            Canvas.SetLeft(player, 190);
+            Canvas.SetTop(player, 288);
+
+            Timer.Start();
+            GameHero = new Hero(7, 7, 15);
+
+        }
+
+        private void Canvas_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.D)
+                GameHero.MoveRight();
+            else if (e.Key == Key.A)
+                GameHero.MoveLeft();
+            else if (e.Key == Key.W)
+                GameHero.Jump();
+        }
+
+        private void Canvas_KeyUp(object sender, KeyEventArgs e)
+        {
+            GameHero.StopMoving();
+        }
+    }
+}
