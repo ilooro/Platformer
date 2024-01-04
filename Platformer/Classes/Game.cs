7e0ea36976ee5c51e2ed713c964d5fab0a8c3955
SoulslikeEnjoyer <paveldzyublo@yandex.ru>
2024-01-05 00:41:50 +0700

    Attempt number 3756

---
+++ Platformer/Classes/Game.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Platformer.Classes {
+    internal class Game(Engine gameEngine, Player gameHero) {
+        #region Attributes
+        //game engine
+        public readonly Engine engine = gameEngine;
+
+        //player
+        public readonly Player hero = gameHero;
+
+        //current level index
+        public uint currentLevel = 0;
+
+        //other entities
+        //private Entity Core;
+        //private List<Enemy> monsters;
+        #endregion
+        #region Methods
+        //level managing
+        public void LoadLevel(MainWindow window, uint LevelIndex = 0) {
+            //todo:
+            //window.Canvas.Children.Clear();
+            switch (LevelIndex) {
+                default:
+                case 0: {
+                        //set hero initial position on canvas
+                        Canvas.SetLeft(window.playerSprite, 190);
+                        Canvas.SetTop(window.playerSprite, 288);
+
+                        //start timer
+                        engine.Timer.Start();
+                        break;
+                    }
+            }
+        }
+        #endregion
+    }
+}
--- Platformer/MainWindow.xaml.cs
+++ Platformer/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
 ﻿using Platformer.Classes;
+<<<<<<< HEAD
+=======
+using System.Reflection;
+using System.Security.RightsManagement;
+>>>>>>> 6902503 (Added Engine and Game classes. Also some sprites for player and enemies.)
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +16,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
+<<<<<<< HEAD
 namespace Platformer
 {
     public partial class MainWindow : Window
@@ -71,5 +77,68 @@ namespace Platformer
         {
             GameHero.StopMoving();
         }
+=======
+namespace Platformer {
+    public partial class MainWindow : Window {
+        #region Attributes
+        //game itself
+        private readonly Game platformer;
+        #endregion
+        #region Methods
+        private void CustomRender(object? sender, EventArgs e) {
+            Rect playerHitBox = new(Canvas.GetLeft(playerSprite), Canvas.GetTop(playerSprite), playerSprite.Width, playerSprite.Height);
+            var boxes = new List<Rect>();
+            IEnumerable<Rectangle> rectangles = Canvas.Children.OfType<Rectangle>();
+            foreach (var rect in rectangles)
+                if (rect != playerSprite)
+                    boxes.Add(new Rect(Canvas.GetLeft(rect), Canvas.GetTop(rect), rect.Width, rect.Height));
+
+            platformer.hero.Update(ref playerHitBox, boxes);
+
+            Canvas.SetTop(playerSprite, Canvas.GetTop(playerSprite) + platformer.hero.CurrentVerOffset);
+            Canvas.SetLeft(playerSprite, Canvas.GetLeft(playerSprite) + platformer.hero.CurrentHorOffset);
+        }
+
+        //callbacks
+        private void CanvasKeyDownCallback(object? sender, KeyEventArgs e) {
+            //basic application options
+            if (e.Key == Key.Escape) {
+                Environment.Exit(0);
+                return;
+            } //quit application
+
+            //basic game inputs
+            if (e.Key == Key.R) {
+                platformer.LoadLevel(this, platformer.currentLevel);
+            } //restart current level
+
+            //player movement
+            if (e.Key == Key.D)
+                platformer.hero.MoveRight();
+            else if (e.Key == Key.A)
+                platformer.hero.MoveLeft();
+            else if (e.Key == Key.W)
+                platformer.hero.Jump();
+        }
+        private void CanvasKeyUpCallback(object? sender, KeyEventArgs e)
+        {
+            //control inputs
+            platformer.hero.StopMoving();
+        }
+
+        //constructors
+        public MainWindow() {
+            //application components initialization
+            InitializeComponent();
+            Canvas.Focus();
+
+            //game engine initialization
+            platformer = new(new(CustomRender), new(7, 7, 15));
+
+            //load first game level (main menu load should be here as level 0)
+            platformer.LoadLevel(this, 0);
+        } //default constructor
+        #endregion
+>>>>>>> 6902503 (Added Engine and Game classes. Also some sprites for player and enemies.)
     }
 }
