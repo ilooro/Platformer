59e2bd483199de9b3707aa90270458d99a33e712
ilooro <ilverrkhv@gmail.com>
2024-01-09 14:03:36 +0700

    Fix server

--- C#TestServer/TestServer/TestServer/obj/Debug/net8.0/TestServer.AssemblyInfo.cs
+++ C#TestServer/TestServer/TestServer/obj/Debug/net8.0/TestServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99811bc08a3d4016a76d30e8276ea50c220d15ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
